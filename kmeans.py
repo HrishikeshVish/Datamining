35e6170a38dbd588134f75f6a9d472894da2f21e
Hrishikesh Viswanath <38471846+HrishikeshVish@users.noreply.github.com>
2021-11-21 15:40:31 -0500

    Add files via upload

--- kmeans.py
+++ kmeans.py
@@ -4,14 +4,15 @@ from statistics import mode, mean
 import copy
 import math
 from scipy.spatial.distance import cdist
+import time
 np.random.seed(0)
 
 data = pd.read_csv('digits-embedding.csv')
 def euclidean_dist(point1, point2):
     dist = 0.0
     for i in range(2, len(point1)):
-        dist += ((point1[i] - point2[i])**2)
-    return dist**0.5
+        dist += np.square(point1[i] - point2[i-2])
+    return np.sqrt(dist)
 def find_ele(element, vector):
     for i in range(len(vector)):
         if element in vector[i]:
@@ -19,11 +20,11 @@ def find_ele(element, vector):
     return -1
 def change_centroid(init_centroids, membershipVector):
     for i in range(len(init_centroids)):
-        membershipVector[i].append(list(init_centroids[i]))
+        #membershipVector[i].append(list(init_centroids[i]))
         coord1 = [j[2] for j in membershipVector[i]]
         coord2 = [j[3] for j in membershipVector[i]]
-        init_centroids[i][2] = mean(coord1)
-        init_centroids[i][3] = mean(coord2)
+        init_centroids[i][0] = mean(coord1)
+        init_centroids[i][1] = mean(coord2)
     return init_centroids
 def dist_in_vector(ele, curVector):
     dist = 0.0
@@ -68,7 +69,7 @@ def calcStuff(membershipVector, init_centroids, data):
         membershipVector[i] = membershipVector[i].drop('id', axis=1)
         membershipVector[i] = membershipVector[i].drop('class', axis=1)
         membershipVector[i] = np.asarray(membershipVector[i])
-    
+    """
     for i in range(len(membershipVector)):
         curVector = membershipVector[i]
         remVectors = copy.copy(membershipVector)
@@ -94,6 +95,7 @@ def calcStuff(membershipVector, init_centroids, data):
         B_vals = [sum(i)/len(i) for i in distances_other]
         for i in range(len(B_vals)):
             S_i.append((B_vals[i] - A_vals[i])/max(A_vals[i], B_vals[i]))
+    """
     silCoef = mean(S_i)            
          
             
@@ -104,43 +106,61 @@ def kmeans(K, data):
     init_centroids = data.iloc[initial_centroid_indices]
     #print(init_centroids)
     indexes = list(init_centroids.index)
-    remaining_points = data.drop(indexes)
+    #remaining_points = data.drop(indexes)
+    remaining_points = data
     init_centroids = np.asarray(init_centroids)
-    print(init_centroids)
+    rem_points = copy.copy(remaining_points)
+    rem_points.set_index(['id', 'class'])
+    rem_points = rem_points.drop('id', axis=1)
+    rem_points = rem_points.drop('class', axis=1)
     remaining_points = np.asarray(remaining_points)
-    
+    membershipDict = {}
     membershipVector = []
     for i in range(K):
         membershipVector.append([])
     vector_euclid_dist = np.vectorize(euclidean_dist, signature='(n),(m)->()')
-    distances = vector_euclid_dist(remaining_points[:,np.newaxis], init_centroids)
+    init_centroids = pd.DataFrame(init_centroids)
+    init_centroids.columns = ['id', 'class', 'x', 'y']
+    init_centroids = init_centroids.drop('id', axis=1)
+    init_centroids = init_centroids.drop('class', axis=1)
+    init_centroids = np.asarray(init_centroids)
+    distances = cdist(rem_points, init_centroids, metric='euclidean')
+    
+
     for i in range(len(distances)):
         min_dist_index = list(distances[i]).index(min(distances[i]))
+        membershipDict[remaining_points[i][0]] = min_dist_index
         membershipVector[min_dist_index].append(list(remaining_points[i]))
     #print(init_centroids)
     for i in range(len(init_centroids)):
-        membershipVector[i].append(list(init_centroids[i]))
         coord1 = [j[2] for j in membershipVector[i]]
         coord2 = [j[3] for j in membershipVector[i]]
-        init_centroids[i][2] = mean(coord1)
-        init_centroids[i][3] = mean(coord2)
-    #print(init_centroids)
+        init_centroids[i][0] = mean(coord1)
+        init_centroids[i][1] = mean(coord2)
+
     changed = True
     for epoch in range(49):
         if changed == False:
             break
         changed_any = False
         count = 0
-        distances = vector_euclid_dist(remaining_points[:, np.newaxis], init_centroids)
-        for i in range(len(distances)):
-            min_dist_index = list(distances[i]).index(min(distances[i]))
-            
-            if(list(remaining_points[i]) not in membershipVector[min_dist_index]):
-                cur_index = find_ele(list(remaining_points[i]), membershipVector)
-                membershipVector[cur_index].remove(list(remaining_points[i]))
-                membershipVector[min_dist_index].append(list(remaining_points[i]))
-                count +=1
-                changed_any = True
+        start = time.time()
+        distances = cdist(rem_points, init_centroids, metric='euclidean')
+        cur_indices = np.asarray(list(membershipDict.values()))
+        min_dist_indices = np.asarray(distances.argmin(axis=1))        
+        changed_indices = np.where(cur_indices != min_dist_indices, 1, 0)
+        min_dist_index = [value for value in np.where(cur_indices != min_dist_indices, min_dist_indices, -1) if value !=-1]
+
+        indices = list(np.where(changed_indices == 1)[0])
+
+        for i in range(len(indices)):
+            cur_index = membershipDict[remaining_points[indices[i]][0]]
+            membershipVector[cur_index].remove(list(remaining_points[indices[i]]))
+            membershipVector[min_dist_index[i]].append(list(remaining_points[indices[i]]))
+            membershipDict[remaining_points[indices[i]][0]] = min_dist_index[i]
+            changed_any = True
+            count+=1
+
         if(epoch == 48):
             break
         if(changed_any == False):
@@ -149,7 +169,10 @@ def kmeans(K, data):
         
         else:
             print("Epoch ", str(epoch), " ", str(count), " Element(s) Changed")
+
             init_centroids = change_centroid(init_centroids, membershipVector)
+
+
             
     wc_ssd, silCoef, nmi = calcStuff(membershipVector, init_centroids, data)
     print("WC-SSD: ",str(wc_ssd))
